4f91e1eedf5f9349ce88e418023d8bbf54dd20ad
Bryan L. Venable <bryanlvenable@gmail.com>
2016-01-09 12:24:09 -0800

    started user.js

---
+++ app/models/user.js
@@ -0,0 +1,14 @@
+// Get all the packages
+var express     = require('express'),
+    app         = express(),
+    bodyParser  = require('bodyParser'),
+    morgan      = require('morgan'),
+    mongoose    = require('mongoose'),
+    jwt         = require('jsonwebtoken')
+    config      = require('./config'),
+    User        = require('./app/models/user');
+
+// Configuration
+var port = process.env.PORT || 9000;
+mongoose.connect(config.database);
+app.set('secret, config.secret');
