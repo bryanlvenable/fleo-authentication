3e3fd5aa67d55dc43b3e1a22b7a45d66829b225f
Bryan L. Venable <bryanlvenable@gmail.com>
2016-01-10 01:17:35 -0800

    server runs

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 // Get all the packages
 var express     = require('express'),
     app         = express(),
-    bodyParser  = require('bodyParser'),
+    bodyParser  = require('body-parser'),
     morgan      = require('morgan'),
     mongoose    = require('mongoose'),
     jwt         = require('jsonwebtoken')
-    config      = require('config/config'),
+    config      = require('./config/config'),
     User        = require('./app/models/user');
 
 // Configuration
-var port = config.port;
+var port = config.port,
+    host = config.host;
+
 mongoose.connect(config.mongo.uri);
-app.set('secret', config.secret);
+app.set('superSecret', config.secret);
+
+// body-parser allows us to get ifo from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+
+// TODO modularize routes
+
+/***** Routes *****/
+app.get('/', function (req, res) {
+    res.send('Hello! Welcome to fleo authentication service.');
+});
+
+app.listen(port);
+console.log('Magic happens at ' + host + port);
