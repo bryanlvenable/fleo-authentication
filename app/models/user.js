b979175d69d4138d550e53f3d04091a11a7529d8
Bryan L. Venable <bryanlvenable@gmail.com>
2016-01-10 00:47:43 -0800

    added user schema

--- app/models/user.js
+++ app/models/user.js
@@ -1,14 +1,9 @@
-// Get all the packages
-var express     = require('express'),
-    app         = express(),
-    bodyParser  = require('bodyParser'),
-    morgan      = require('morgan'),
-    mongoose    = require('mongoose'),
-    jwt         = require('jsonwebtoken')
-    config      = require('config/config'),
-    User        = require('./app/models/user');
+var mongoose = require('mongoose');
 
-// Configuration
-var port = process.env.PORT || 9000;
-mongoose.connect(config.database);
-app.set('secret, config.secret');
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('User', new Schema({
+    name: String,
+    password: String,
+    admin: Boolean
+}));
